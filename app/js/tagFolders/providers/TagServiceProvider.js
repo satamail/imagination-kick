c2c50d9bc40b802d908a00086bb71f1f57fcd07a
Ihar Kryvonos <Ihar_Kryvonos@epam.com>
2015-04-03 13:29:24 +0300

    Create TagServiceProvider

---
+++ app/js/tagFolders/providers/TagServiceProvider.js
@@ -0,0 +1,52 @@
+"use strict";
+/* Implimintation of tag service provider, which contains tag service config */
+
+(function(angular) {
+    function TagServiceProvider()
+    {
+        var self = this;
+
+        var tagSevice = undefined;
+
+        var resourceUrl = undefined;
+
+        var resourceMethods = undefined;
+
+        this.setTagResourceUrl = function setTagResource(tagResourcUrl)
+        {
+            resourceUrl = tagResourcUrl;
+            return self;
+        }
+
+        this.setTagResourceMethods = function setTagResourceMethods(tagResourceMethods)
+        {
+            resourceMethods = tagResourceMethods;
+            return self;
+        }
+
+        this.$get = ['$resource', function($resource)
+        {
+            if (angular.isUndefined(tagSevice))
+            {
+                console.log(resourceUrl, null, resourceMethods);
+                var tagResource = $resource(resourceUrl, null, resourceMethods);
+                tagSevice = new TagService(tagResource);
+            }
+            return tagSevice;
+        }];
+    }
+
+    function TagService(tagResource) {
+        this.getTagList = function(args)
+        {
+            var defaults = {
+                count: 10
+            }
+            args = angular.extend(defaults, args);
+            return tagResource.query(args);
+        }
+    }
+
+    angular.module('TagFolders')
+        .provider('TagService', TagServiceProvider);
+})(angular);
