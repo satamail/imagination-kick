998b2edf0f7d5a2ec1c1f29017f04e2be5055d7a
Ihar Kryvonos <Ihar_Kryvonos@epam.com>
2015-04-06 11:35:36 +0300

    Change karma.conf

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config){
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/js/imgUtils/main.js',
       'app/js/TagCatalog/main.js',
-      'app/js/imaginationKick/main.js',
+      'app/js/ImaginationKick/main.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
     ],
