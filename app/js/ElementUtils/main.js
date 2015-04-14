ac722dfe832d9dcc50c28332e873838b6b713bb1
Ihar Kryvonos <Ihar_Kryvonos@epam.com>
2015-04-14 16:14:20 +0300

    Modify ElementUtils and create unit tests

--- app/js/ElementUtils/main.js
+++ app/js/ElementUtils/main.js
@@ -3,16 +3,18 @@
 (function(angular) {
     function ElementUtils($rootElement)
     {
-        this.getElementByAttr = function getElementByAttr(attr, context)
+        var self = this;
+
+        self.getElementByAttr = function getElementByAttr(attr, context)
         {
             var nodeList = (context || $rootElement).find('*');
-            var nodeArray = [];
+            var nodeArray = angular.element();
             var node = null;
 
             for(var i = 0; i < nodeList.length; i++)
             {
-                node = nodeList[i++];
-                if (node.attr(attribute))
+                node = nodeList.eq(i);
+                if (self.hasAttribute(node, attr))
                 {
                     nodeArray.push(node);
                 }
@@ -20,7 +22,34 @@
 
             return nodeArray;
         }
+
+        self.hasAttribute = function hasAttribute(element, attribute)
+        {
+            var attr = element.attr(attribute);
+            return (typeof attr !== typeof undefined && attr !== false)
+        }
+
+        self.hasAttributeFromList = function hasAttributeFromList(element, attributeList)
+        {
+            var hasAttribute = [];
+            angular.forEach(attributeList, function searchAttributeFromList(attrName)
+            {
+                if (self.hasAttribute(element, attrName))
+                {
+                    hasAttribute.push(attrName);
+                }
+            });
+            return hasAttribute;
+        }
+
+        self.removeAll = function removeAll(elementList)
+        {
+            angular.forEach(elementList, function removeElement(element)
+            {
+                element.remove();
+            });
+        }
     }
-    angular.module('ElementUtils')
+    angular.module('ElementUtils', [])
     .service('ElementUtils', ['$rootElement', ElementUtils]);
 })(angular)
